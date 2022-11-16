e814ac1c0a1b9aecd9c12d7e4a6a5afdf2fdddd1
Samuel Park <s.park111497@gmail.com>
2022-11-16 13:47:37 -0800

    added api router module

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,21 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 
-router.get('/hello/world', (req, res) => {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
+const apiRouter = require('./api');
+
+// router.get('/hello/world', (req, res) => {
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.json('Hello World!')
+// });
+
+router.get('/api/csrf/restore', (req, res, next) => {
+  const csrfToken = req.csrfToken();
+  res.cookie('XSRF-TOKEN', csrfToken);
+  res.status(200).json({
+    'XSRF-Token': csrfToken
+  });
 });
+
+router.use('/api', apiRouter);
 
 module.exports = router;
 
